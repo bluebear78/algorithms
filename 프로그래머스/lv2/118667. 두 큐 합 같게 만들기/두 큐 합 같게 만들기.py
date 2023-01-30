e5f9ae5999666cc590a6d1b328ce0c8c7899e32d
bluebear78 <30998582+bluebear78@users.noreply.github.com>
2023-01-30 13:32:52 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 54.72 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -5,22 +5,23 @@ def solution(queue1, queue2):
     queue1 = deque(queue1)
     queue2 = deque(queue2)
 
-    sum1,sum2 = sum(queue1),sum(queue2)
+    maximium = len(queue1)+len(queue2)
+    while True:
+        sum_1 = sum(queue1)
+        sum_2 = sum(queue2)
 
-    for _ in range(3*len(queue1)):
-        if sum1 > sum2:
-            sum1 -= queue1[0]
-            sum2 += queue1[0]
-            queue2.append(queue1.popleft())
-        elif sum1<sum2:
-            sum1 += queue2[0]
-            sum2 -= queue2[0]
-            queue1.append(queue2.popleft())
+        if answer>maximium:
+            return -1
+
+        if sum_1 == sum_2:
+            break
+        elif sum_1 < sum_2:
+            temp = queue2.popleft()
+            queue1.append(temp)
         else:
-            return answer
-        answer += 1
-    return -1
-
+            temp = queue1.popleft()
+            queue2.append(temp)
+        answer+=1
 
 
 
